a07b88d9421ce415155c44c1fdce2af46179cbd4
backkem <mail@backkem.me>
2019-01-19 21:37:12 +0100

    Add E2E test
    Resolves #3

--- association_test.go
+++ association_test.go
@@ -2,7 +2,12 @@ package sctp
 
 import (
 	"fmt"
+	"net"
+	"sync"
 	"testing"
+	"time"
+
+	"github.com/pions/transport/test"
 )
 
 func TestAssociationInit(t *testing.T) {
@@ -20,112 +25,202 @@ func TestAssociationInit(t *testing.T) {
 	}
 }
 
-// TODO: find a good way to avoid deadlocking in the test
-// func TestStressDuplex(t *testing.T) {
-// 	// Limit runtime in case of deadlocks
-// 	lim := test.TimeOut(time.Second * 20)
-// 	defer lim.Stop()
-//
-// 	// Check for leaking routines
-// 	report := test.CheckRoutines(t)
-// 	defer report()
-//
-// 	// Run the test
-// 	stressDuplex(t)
-// }
-//
-// func stressDuplex(t *testing.T) {
-// 	lim := test.TimeOut(time.Second * 5)
-// 	defer lim.Stop()
-//
-// 	ca, cb, stop, err := pipeMemory()
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-//
-// 	defer stop(t)
-//
-// 	opt := test.Options{
-// 		MsgSize:  2048,
-// 		MsgCount: 50,
-// 	}
-//
-// 	err = test.StressDuplex(ca, cb, opt)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// }
-//
-// func pipeMemory() (*Stream, *Stream, func(*testing.T), error) {
-// 	var err error
-//
-// 	var aa, ab *Association
-// 	aa, ab, err = associationMemory()
-// 	if err != nil {
-// 		return nil, nil, nil, err
-// 	}
-//
-// 	var sa, sb *Stream
-// 	sa, err = aa.OpenStream(0, 0)
-// 	if err != nil {
-// 		return nil, nil, nil, err
-// 	}
-//
-// 	sb, err = ab.OpenStream(0, 0)
-// 	if err != nil {
-// 		return nil, nil, nil, err
-// 	}
-//
-// 	stop := func(t *testing.T) {
-// 		err = sa.Close()
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
-// 		err = sb.Close()
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
-// 		err = aa.Close()
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
-// 		err = ab.Close()
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
-// 	}
-//
-// 	return sa, sb, stop, nil
-// }
-//
-// func associationMemory() (*Association, *Association, error) {
-// 	// In memory pipe
-// 	ca, cb := test.PacketPipe(100) // TODO: Find a better way to avoid blocking
-//
-// 	type result struct {
-// 		a   *Association
-// 		err error
-// 	}
-//
-// 	c := make(chan result)
-//
-// 	// Setup client
-// 	go func() {
-// 		client, err := Client(ca)
-// 		c <- result{client, err}
-// 	}()
-//
-// 	// Setup server
-// 	server, err := Server(cb)
-// 	if err != nil {
-// 		return nil, nil, err
-// 	}
-//
-// 	// Receive client
-// 	res := <-c
-// 	if res.err != nil {
-// 		return nil, nil, res.err
-// 	}
-//
-// 	return res.a, server, nil
-// }
+func TestStressDuplex(t *testing.T) {
+	// Limit runtime in case of deadlocks
+	lim := test.TimeOut(time.Second * 20)
+	defer lim.Stop()
+
+	// Check for leaking routines
+	report := test.CheckRoutines(t)
+	defer report()
+
+	stressDuplex(t)
+}
+
+func stressDuplex(t *testing.T) {
+	ca, cb, stop, err := pipe(pipeDump)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer stop(t)
+
+	// TODO: Increase once SCTP is more reliable in case of slow reader
+	opt := test.Options{
+		MsgSize:  2048, // 65535,
+		MsgCount: 10,   // 1000,
+	}
+
+	err = test.StressDuplex(ca, cb, opt)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func pipe(piper piperFunc) (*Stream, *Stream, func(*testing.T), error) {
+	var err error
+
+	var aa, ab *Association
+	aa, ab, err = association(piper)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	var sa, sb *Stream
+	sa, err = aa.OpenStream(0, 0)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	sb, err = ab.OpenStream(0, 0)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	stop := func(t *testing.T) {
+		err = sa.Close()
+		if err != nil {
+			t.Error(err)
+		}
+		err = sb.Close()
+		if err != nil {
+			t.Error(err)
+		}
+		err = aa.Close()
+		if err != nil {
+			t.Error(err)
+		}
+		err = ab.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	return sa, sb, stop, nil
+}
+
+func association(piper piperFunc) (*Association, *Association, error) {
+	ca, cb := piper()
+
+	type result struct {
+		a   *Association
+		err error
+	}
+
+	c := make(chan result)
+
+	// Setup client
+	go func() {
+		client, err := Client(ca)
+		c <- result{client, err}
+	}()
+
+	// Setup server
+	server, err := Server(cb)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Receive client
+	res := <-c
+	if res.err != nil {
+		return nil, nil, res.err
+	}
+
+	return res.a, server, nil
+}
+
+type piperFunc func() (net.Conn, net.Conn)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func pipeDump() (net.Conn, net.Conn) {
+	aConn := acceptDumbConn()
+
+	bConn, err := net.DialUDP("udp4", nil, aConn.LocalAddr().(*net.UDPAddr))
+	check(err)
+
+	// Dumb handshake
+	mgs := "Test"
+	_, err = bConn.Write([]byte(mgs))
+	check(err)
+
+	b := make([]byte, 4)
+	_, err = aConn.Read(b)
+	check(err)
+
+	if string(b) != mgs {
+		panic("Dumb handshake failed")
+	}
+
+	return aConn, bConn
+}
+
+type dumbConn struct {
+	mu    sync.RWMutex
+	rAddr net.Addr
+	pConn net.PacketConn
+}
+
+func acceptDumbConn() *dumbConn {
+	pConn, err := net.ListenUDP("udp4", nil)
+	check(err)
+	return &dumbConn{
+		pConn: pConn,
+	}
+}
+
+// Read
+func (c *dumbConn) Read(p []byte) (int, error) {
+	i, rAddr, err := c.pConn.ReadFrom(p)
+	if err != nil {
+		return 0, err
+	}
+
+	c.mu.Lock()
+	c.rAddr = rAddr
+	c.mu.Unlock()
+
+	return i, err
+}
+
+// Write writes len(p) bytes from p to the DTLS connection
+func (c *dumbConn) Write(p []byte) (n int, err error) {
+	return c.pConn.WriteTo(p, c.RemoteAddr())
+}
+
+// Close closes the conn and releases any Read calls
+func (c *dumbConn) Close() error {
+	return c.pConn.Close()
+}
+
+// LocalAddr is a stub
+func (c *dumbConn) LocalAddr() net.Addr {
+	return c.pConn.LocalAddr()
+}
+
+// RemoteAddr is a stub
+func (c *dumbConn) RemoteAddr() net.Addr {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.rAddr
+}
+
+// SetDeadline is a stub
+func (c *dumbConn) SetDeadline(t time.Time) error {
+	return nil
+}
+
+// SetReadDeadline is a stub
+func (c *dumbConn) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+// SetWriteDeadline is a stub
+func (c *dumbConn) SetWriteDeadline(t time.Time) error {
+	return nil
+}
