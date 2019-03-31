0fa6e4f273a3c855024c868011ab735b247fb68a
Yutaka Takeda <yt0916@gmail.com>
2019-03-30 23:05:06 -0700

    Fixed a bug found by datachannel tests
    Optimized the pending queue.
    Resolves #11

--- pending_queue.go
+++ pending_queue.go
@@ -1,34 +1,90 @@
 package sctp
 
-type pendingQueue struct {
-	queue  []*chunkPayloadData
-	nBytes int
+import (
+	"fmt"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+// pendingBaseQueue
+
+type pendingBaseQueue struct {
+	queue []*chunkPayloadData
 }
 
-func newPendingQueue() *pendingQueue {
-	return &pendingQueue{queue: []*chunkPayloadData{}}
+func newPendingBaseQueue() *pendingBaseQueue {
+	return &pendingBaseQueue{queue: []*chunkPayloadData{}}
 }
 
-func (q *pendingQueue) push(c *chunkPayloadData) {
+func (q *pendingBaseQueue) push(c *chunkPayloadData) {
 	q.queue = append(q.queue, c)
-	q.nBytes += len(c.userData)
 }
 
-func (q *pendingQueue) pop() *chunkPayloadData {
+func (q *pendingBaseQueue) pop() *chunkPayloadData {
 	if len(q.queue) == 0 {
 		return nil
 	}
 	c := q.queue[0]
 	q.queue = q.queue[1:]
-	q.nBytes -= len(c.userData)
 	return c
 }
 
-func (q *pendingQueue) get(i int) *chunkPayloadData {
+func (q *pendingBaseQueue) get(i int) *chunkPayloadData {
 	if len(q.queue) == 0 || i < 0 || i >= len(q.queue) {
 		return nil
 	}
 	return q.queue[i]
+}
+
+func (q *pendingBaseQueue) size() int {
+	return len(q.queue)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// pendingQueue
+
+type pendingQueue struct {
+	unorderedQueue *pendingBaseQueue
+	orderedQueue   *pendingBaseQueue
+	nBytes         int
+}
+
+func newPendingQueue() *pendingQueue {
+	return &pendingQueue{
+		unorderedQueue: newPendingBaseQueue(),
+		orderedQueue:   newPendingBaseQueue(),
+	}
+}
+
+func (q *pendingQueue) push(c *chunkPayloadData) {
+	if c.unordered {
+		q.unorderedQueue.push(c)
+	} else {
+		q.orderedQueue.push(c)
+	}
+	q.nBytes += len(c.userData)
+}
+
+func (q *pendingQueue) peek() *chunkPayloadData {
+	if c := q.unorderedQueue.get(0); c != nil {
+		return c
+	}
+	return q.orderedQueue.get(0)
+}
+
+func (q *pendingQueue) pop(c *chunkPayloadData) error {
+	if c.unordered {
+		popped := q.unorderedQueue.pop()
+		if popped != c {
+			return fmt.Errorf("unexped chunk popped (unordered)")
+		}
+	} else {
+		popped := q.orderedQueue.pop()
+		if popped != c {
+			return fmt.Errorf("unexped chunk popped (ordered)")
+		}
+	}
+	q.nBytes -= len(c.userData)
+	return nil
 }
 
 func (q *pendingQueue) getNumBytes() int {
@@ -36,5 +92,5 @@ func (q *pendingQueue) getNumBytes() int {
 }
 
 func (q *pendingQueue) size() int {
-	return len(q.queue)
+	return q.unorderedQueue.size() + q.orderedQueue.size()
 }
