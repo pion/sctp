292702583961ecd98358283baee11b6330e37bb0
Sean DuBois <sean@siobud.com>
2024-02-08 15:38:37 -0500

    Support Unknown Parameters in INIT Chunk
    Start to implement section-3.2.1 and respect chunks with a action
    of Skip. We don't properly report the values back yet.
    
    Relates to #2618

---
+++ chunk_init_test.go
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+package sctp
+
+import (
+	"testing"
+)
+
+func TestChunkInit_UnrecognizedParameters(t *testing.T) {
+	initChunkHeader := []byte{
+		0x55, 0xb9, 0x64, 0xa5, 0x00, 0x02, 0x00, 0x00,
+		0x04, 0x00, 0x08, 0x00, 0xe8, 0x6d, 0x10, 0x30,
+	}
+
+	unrecognizedSkip := append([]byte{}, initChunkHeader...)
+	unrecognizedSkip = append(unrecognizedSkip, byte(paramHeaderUnrecognizedActionSkip), 0xFF, 0x00, 0x04, 0x00)
+
+	i := &chunkInitCommon{}
+	if err := i.unmarshal(unrecognizedSkip); err != nil {
+		t.Errorf("Unmarshal init Chunk failed: %v", err)
+	} else if len(i.unrecognizedParams) != 1 || i.unrecognizedParams[0].unrecognizedAction != paramHeaderUnrecognizedActionSkip {
+		t.Errorf("Unrecognized Param parsed incorrectly")
+	}
+
+	unrecognizedStop := append([]byte{}, initChunkHeader...)
+	unrecognizedStop = append(unrecognizedStop, byte(paramHeaderUnrecognizedActionStop), 0xFF, 0x00, 0x04, 0x00)
+
+	i = &chunkInitCommon{}
+	if err := i.unmarshal(unrecognizedStop); err != nil {
+		t.Errorf("Unmarshal init Chunk failed: %v", err)
+	} else if len(i.unrecognizedParams) != 1 || i.unrecognizedParams[0].unrecognizedAction != paramHeaderUnrecognizedActionStop {
+		t.Errorf("Unrecognized Param parsed incorrectly")
+	}
+}
