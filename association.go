87f8adf53d7c9bcaff97b81ed24659966ec25de4
Lanius-collaris <55432068+Lanius-collaris@users.noreply.github.com>
2026-03-13 00:53:19 +0800

    reduce incorrect delayed ACKs

--- association.go
+++ association.go
@@ -179,6 +179,7 @@ func getAssociationStateString(assoc uint32) string {
 type Association struct {
 	bytesReceived uint64
 	bytesSent     uint64
+	unackedBytes  uint32
 
 	lock sync.RWMutex
 
@@ -1921,6 +1922,11 @@ func (a *Association) handleData(chunkPayload *chunkPayloadData) []*packet {
 	gapDetected := sna32GT(chunkPayload.tsn, expectedTSN)
 
 	sackNow := chunkPayload.immediateSack || gapDetected
+	a.unackedBytes += uint32(len(chunkPayload.userData))
+	if a.unackedBytes >= 2*a.MTU() {
+		a.unackedBytes = 0
+		sackNow = true
+	}
 
 	return a.handlePeerLastTSNAndAcknowledgement(sackNow)
 }
